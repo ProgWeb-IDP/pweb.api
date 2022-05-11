2b2bf7b58328f7a03e11b41a26582c0472c5bee8
Udroiu Ruben <47859590+UdroiuRuben@users.noreply.github.com>
2022-05-11 03:25:17 +0300

    added BE base

---
+++ HelpARefugee/Controllers/UserController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using HelpARefugee.Models;
+
+namespace HelpARefugee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select userId, firstName, lastName from dbo.Users";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Users  user)
+        {
+            string query = @"
+                        insert into dbo.Users (firstName, lastName) values
+                        (
+                            '" + user.firstName + @"',
+                            '" + user.lastName + @"'
+                        )";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+        
+        [HttpPut]
+        public JsonResult Put(Users  user)
+        {
+            string query = @"update dbo.Users set
+                            firstName = '" + user.firstName + @"',
+                            lastName = '" + user.lastName + @"'
+                            where userId = " + user.userId + @"
+                            ";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from dbo.Users where userId = " + id + @"";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ HelpARefugee/Models/Users.cs
@@ -0,0 +1,33 @@
+﻿namespace HelpARefugee.Models
+{
+    public class Users
+    {
+        public int userId { get; set; }
+
+        public int isVolunteer { get; set; }
+
+        public int isAdmin { get; set; }
+
+        public string firstName { get; set; } 
+
+        public string lastName { get; set; } 
+
+        public string birthDate { get; set; }
+
+        public string gender { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string country { get; set; }
+
+        public string city { get; set; }
+
+        public string street { get; set; } 
+
+        public string address { get; set; }
+
+        public string zipCode { get; set; }
+
+        public string registerDate { get; set; }
+    }
+}
